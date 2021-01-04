e2b0b30f150687fa547477b89585bb013cbfa1a0
Mesaque Batista de Oliveira <bmesaque@outlook.com>
2021-01-04 20:35:46 -0300

    Inicio STORAGE controller

--- app/Http/Controllers/VisitantsController.php
+++ app/Http/Controllers/VisitantsController.php
@@ -13,6 +13,24 @@ class VisitantsController extends Controller
     }
 
     public function store(Request $request){
+
+        $validatedData = $request->validate($this->validations());
+
+        // TODO: Requisição a API do CEP
+
         $visitant = new Visitant;
+        $visitant->name = $validatedData['name'];
+        $visitant->email = $validatedData['email'];
+        $visitant->nasciemnto = ['nascimento'];
+
+    }
+
+    public function validations(){
+        return [
+            'name'      => ['required'],
+            'cep'       => ['required'],
+            'email'     => ['unique:visitants'],
+            'nascimento'=> ['required']
+        ];
     }
 }
--- app/Models/Visitant.php
+++ app/Models/Visitant.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Visitant extends Model
 {
     use HasFactory;
-    $table = "visitants";
+    protected $table = "visitants";
 }
