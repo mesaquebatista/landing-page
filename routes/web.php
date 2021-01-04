a4f90872d701c3d176c28565cb4bec434fc5e897
Mesaque Batista de Oliveira <bmesaque@outlook.com>
2021-01-04 20:17:38 -0300

    ALterando a rota Index

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VisitantsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [VisitantsController::class, 'index']);
